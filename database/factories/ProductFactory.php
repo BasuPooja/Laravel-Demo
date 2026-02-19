54185e5d06c6bd89d8eff8453de31d52d9bbbacc
Pooja Kumari <basupooja306@gmail.com>
2026-02-19 18:17:39 +0530

    Image change issue solved on refreshing browser.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,9 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Product; 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
  */
@@ -32,12 +35,26 @@ class ProductFactory extends Factory
         //     false 
         // );
 
+        // Generate random image ID (stable image)
+        $imageId = "https://picsum.photos/640/480";
+
+        // Fetch image from picsum
+        $imageContents = file_get_contents($imageId);
+
+        // Generate unique filename
+        $fileName = Str::uuid() . '.jpg';
+
+        // Store inside storage/app/public/products
+        Storage::disk('public')->put("products/{$fileName}", $imageContents);
+
         return [
             'name' => $this->faker->words(2, true),
             'price' => $this->faker->numberBetween(100, 5000),
             'description' => $this->faker->sentence(10),
             // 'image' =>'https://picsum.photos/640/480?random=' . rand(1, 10000),
-            'image' =>'https://picsum.photos/640/480?random=' . rand(1, 10000),
+            // 'image' => 'https://picsum.photos/id/' . rand(1, 1000) . '/640/480',
+            // 'image' => $this->faker->imageUrl(640, 480, 'products', true),
+            'image' => "products/{$fileName}",
 
         ];
     }
